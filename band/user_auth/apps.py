0bc945e9476f2a3a35f864dbd9202ddbc690c218
Clever <clevermabunda@gmail.com>
2024-10-30 13:50:04 +0200

    user_auth/apps.py

--- band/user_auth/apps.py
+++ band/user_auth/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class UserAuthConfig(AppConfig):
+    """
+    Configuration for the 'user_auth' application.
+
+    This class manages the configuration settings for the 'user_auth'
+    Django application, including the default primary key field type 
+    and the application name.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_auth'
